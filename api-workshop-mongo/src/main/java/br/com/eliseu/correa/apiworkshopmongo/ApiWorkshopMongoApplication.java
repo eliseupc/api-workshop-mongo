36898de5c622eac6ed267de93f72b23908afb759
Eliseu Correa <eliseupaulinocorrea@hotmail.com>
2022-09-24 22:44:58 -0300

    Primeiro commit do projeto

---
+++ api-workshop-mongo/src/main/java/br/com/eliseu/correa/apiworkshopmongo/ApiWorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.eliseu.correa.apiworkshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWorkshopMongoApplication.class, args);
+	}
+
+}
---
+++ api-workshop-mongo/src/test/java/br/com/eliseu/correa/apiworkshopmongo/ApiWorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eliseu.correa.apiworkshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
